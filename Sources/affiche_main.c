c4a09b044c32efbe852746826da57dc2d4d84f88
jbd14 <dubois.jbaptiste@gmail.com>
2014-10-22 16:19:35 +0200

    Update and rename affiche_main.h to affiche_main.c

--- Sources/affiche_main.c
+++ Sources/affiche_main.c
@@ -1,11 +1,20 @@
 void afficher_mains(short joueur)
 {
-  int i,j;
+  int i;
   for(i=0; i<cartes[52]; i++)
     {
-    if(cartes[i]==joueur)
+    if(cartes[i]==JOUEUR)
       {
         afficher_carte(num);
       }
     }
+}
+
+int main()
+{
+  int i;
+for(i=0;i<JOUEUR;i++){
+afficher_mains(joueur)
+}
+return 0;
 }
