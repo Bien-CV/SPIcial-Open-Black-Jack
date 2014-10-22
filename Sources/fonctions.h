6895dfefcd12a604cbfa7e92a11640f2238ccc00
Bien-CV <Baptiste.cv@gmail.com>
2014-10-22 16:44:18 +0200

    Create fonctions.h

---
+++ Sources/fonctions.h
@@ -0,0 +1,6 @@
+short tirer_carte(short joueur);
+void afficher_carte(short num);
+void afficher_mains(short joueur);
+afficher_mains_cachee(void);
+short donner_valeur_carte(short joueur, short carte);
+short evaluer_score (short joueur, short carte_recue, short *score);
