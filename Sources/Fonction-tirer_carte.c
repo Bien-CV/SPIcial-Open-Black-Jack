fa0e13b096a5155a284b29dce1c28c4f697ac94c
Chewbbibacca <arthur.lemee@sfr.fr>
2014-10-22 16:22:03 +0200

    Update Fonction-tirer_carte.c

--- Sources/Fonction-tirer_carte.c
+++ Sources/Fonction-tirer_carte.c
@@ -1,3 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NB_CARTES 52
+#define LIBRE 0
+#define BANQUE 1
+#define JOUEUR 2
+#define BANQUE_CACHEE 3
+#define DEB_COEURS 0
+#define FIN_COEURS 12
+#define DEB_CARREAUX 13
+#define FIN_CARREAUX 25
+#define DEB_PIQUES 26
+#define FIN_PIQUES 38
+#define DEB_TREFLES 39
+#define FIN_TREFLES 51
+short cartes[52];
+short nb_as_joueur;
+short nb_as_banque;
 
 
 short tirer_carte(short joueur)
