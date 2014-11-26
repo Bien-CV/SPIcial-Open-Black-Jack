032ac1d679f79722326e88c28d12003c982756e5
Yann Guenver <sabrac72@gmail.com>
2014-11-26 09:28:35 +0100

    Update fonctions.h

--- Sources/final/fonctions.h
+++ Sources/final/fonctions.h
@@ -1,3 +1,15 @@
+/**
+* \file fonction.h
+* \brief Librairie des fonctions
+* \author Arthur LEMEE, Yann GUENVER, Jean-Baptiste DUBOIS, Baptiste CANOVAS-VIRLY.
+* \version 1.0
+* \date 22 octobre 2014
+*
+* Librairie pour le programme "Black Jack"
+*
+*/
+
+//Prototype des fonctions
 short tirer_carte(short joueur);
 void afficher_carte(short num);
 void afficher_mains(short joueur);
